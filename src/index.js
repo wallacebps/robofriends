37343f4425434d6caf260c3b9ec265e0c41098f0
Wallace Bruno <wallace.bruno@geocode.com.br>
2024-11-17 11:33:18 -0300

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./containers/App";
-import reportWebVitals from "./reportWebVitals";
-import "tachyons";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware, combineReducers} from 'redux'
+import { createLogger } from 'redux-logger'
+import { thunk } from 'redux-thunk'
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
